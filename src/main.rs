c44ec376711fd8acb2d14ca401168ca985edf46b
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2020-12-16 20:33:14 +0300

    add qsort boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let mut vector = vec![1,2,6,7,3,2,3,4,6,16,22,1,0];
+    q_sort(&mut vector);
+    println!("{:?}",vector);
+}
+
+fn q_sort(vector: &mut Vec<i32>) {
+    iterate_q_sort(vector, 0, vector.len() - 1);
+}
+
+fn iterate_q_sort(vector: &mut Vec<i32>, first:usize, last: usize){
+
+    let middle = last/2;
+    iterate_q_sort(vector, 0, middle - 1 );
+    iterate_q_sort(vector, middle + 1, last);
+}
+
+fn bubble_sort(vector: &mut Vec<i32>) {
+
 }
